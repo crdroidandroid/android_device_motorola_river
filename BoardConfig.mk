e25d063c6989b56283595e172f74cea8e1fa7af0
L1ghtzin <lightzin021@gmail.com>
2025-02-26 18:13:43 -0300

    river: Cleanup & Organize

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,7 +27,7 @@ TARGET_OTA_ASSERT_DEVICE := river
 TARGET_SCREEN_DENSITY := 420
 
 # HIDL
-DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
 
 # Kernel
 TARGET_KERNEL_CONFIG := river_defconfig
